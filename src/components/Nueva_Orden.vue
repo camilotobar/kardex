a0cd884c45f9cb1f81b0c65c1cffcf909adfe49a
dianaitr <dianatorresrojas@hotmail.com>
2019-10-27 19:52:22 -0600

    Formulario hecho falta conectarlo bien

--- src/components/Nueva_Orden.vue
+++ src/components/Nueva_Orden.vue
@@ -13,7 +13,67 @@
             <v-card-title>
              <h2 > Nueva Orden </h2>   
             </v-card-title> 
-            //content
+            <v-form>
+                <v-container >
+                    <v-row justify="center">
+                        <v-col cols="5" sm="6" md="3">
+                            <p class="text-left">
+                                Fecha :
+                            </p>
+                        </v-col>
+                        <v-col cols="5" sm="6" md="3">
+                            <v-text-field
+                                label="Outlined"
+                                outlined
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <v-row justify="center">
+                        <v-col cols="5" sm="6" md="3">
+                        <p class="text-left">
+                            Unidades :
+                        </p>
+                        </v-col>
+                        <v-col cols="5" sm="6" md="3">
+                            <v-text-field
+                                label="Outlined"
+                                outlined
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <v-row justify="center">
+                        <v-col cols="5" sm="6" md="3">
+                            <p class="text-left">
+                                Valor total :
+                            </p>
+                        </v-col>
+                        <v-col cols="5" sm="6" md="3">
+                            <v-text-field
+                                label="Outlined"
+                                outlined
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+
+                    <v-row justify="center">
+                        <v-col cols="5" sm="6" md="3">
+                        <p class="text-left">
+                            Tipo de movimiento :
+                        </p>
+                        </v-col>
+                        <v-col cols="5" sm="6" md="3">
+                            <p class="text-left">
+                            ////poner metodo escogido
+                            </p>
+                        </v-col>
+                    </v-row>
+
+                    <v-btn>Agregar</v-btn>
+
+                </v-container>
+            </v-form>
         </v-card>
     </v-dialog>
 </template>
